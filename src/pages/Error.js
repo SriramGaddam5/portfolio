4423c3299d20308cb8fa76f8f28cbbf009ee9d59
Sriram Gaddam <sriramgaddam5@gmail.com>
2023-11-10 12:52:49 -0600

    add basic home page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,28 +1,41 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Error() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          See source code&nbsp;
-          <a
-            href="https://github.com/SriramGaddam5/portfolio"
-            className="group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
-            <p className={`font-bold text-cyan-400`}>here</p>
-          </a>
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.js</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
-            By Sriram Gaddam
-          </p>
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer">
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className={`text-6xl font-semibold`}>Sriram Gaddam's Portfolio </h1>
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -32,17 +45,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            VIST 105{" "}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-75`}>
-            Principles of Design I
-          </p>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Two-dimensional design; fundamentals of line, color, form, texture,
-            shape, space and arrangement.
+            Find in-depth information about Next.js features and API.
           </p>
         </a>
 
@@ -52,18 +61,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            ARTS 115{" "}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-75`}>
-            Draw for Visualization
-          </p>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Investigation of and practice with media, methods and techniques in
-            communication of design; observational drawing; proportion, form,
-            line and value.
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
@@ -73,13 +77,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Paintings{" "}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Acrylic on canvas paintings
+            Explore starter templates for Next.js.
           </p>
         </a>
 
@@ -89,13 +93,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Other{" "}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Other works of art
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
