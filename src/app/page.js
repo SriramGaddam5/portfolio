44b99b9462a445ecd07caff35f71ff0ad25f72e1
Sriram Gaddam <sriramgaddam5@gmail.com>
2023-11-10 13:08:53 -0600

    clean up a little

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,13 +7,11 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           See source code&nbsp;
-          <a
-            href="https://github.com/SriramGaddam5/portfolio"
-            className="group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer">
-            <p className={`font-bold text-cyan-400`}>here</p>
-          </a>
+          <Link
+            className={`text-cyan-500 font-bold`}
+            href="https://github.com/SriramGaddam5/portfolio">
+            here
+          </Link>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
